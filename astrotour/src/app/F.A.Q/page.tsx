385658f4926f7a5dc7ff816bee5d84c896f90d12
Isti-k <kiscserkes03@gmail.com>
2025-03-06 15:27:14 +0100

    localhost fix

--- astrotour/src/app/F.A.Q/page.tsx
+++ astrotour/src/app/F.A.Q/page.tsx
@@ -10,7 +10,7 @@ const FAQPage = () => {
   useEffect(() => {
     const fetchFAQs = async () => {
       try {
-        const response = await fetch("localhost:8000/api/faq");
+        const response = await fetch("http://localhost:8000/faq");
         if (!response.ok) {
           throw new Error("Hálózati hiba történt.");
         }
